f45bd930f81aad451b0fa2abe43efb21cf491c6e
Noah Williams <thecrazymancan@gmail.com>
2011-07-20 19:24:17 -0500

    added a script for packaging the program

---
+++ package.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+cd ..
+cp backup backup.0.1.0-all
+gzip doc/*
+mkdir usr
+mkdir usr/share
+mkdir usr/share/man
+mkdir usr/share/man/man{1..9}
+find doc -type f -regex '^.*\.[0-9]$' -exec mv '{}' usr/share/man/ \;
+
+mv usr/share/man/*.1 /usr/share/man/man1
+mv usr/share/man/*.2 /usr/share/man/man2
+mv usr/share/man/*.3 /usr/share/man/man3
+mv usr/share/man/*.4 /usr/share/man/man4
+mv usr/share/man/*.5 /usr/share/man/man5
+mv usr/share/man/*.6 /usr/share/man/man6
+mv usr/share/man/*.7 /usr/share/man/man7
+mv usr/share/man/*.8 /usr/share/man/man8
+mv usr/share/man/*.9 /usr/share/man/man9
+
+mkdir usr/bin
+mv src/backup usr/bin
+
+rmdir doc
+rmdir src
+rm package.sh
+rm -rf .git
